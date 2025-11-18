b295fc481c2c40fe4fbc0222627ef18ace1c0bdc
Isa Flores <65521282+Algunaisa@users.noreply.github.com>
2025-11-18 17:41:21 -0600

    Add

---
+++ UnitTesting/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(UTCalculator.CalculatorFeatures.AddTwoNumbers(10, 4)); // Output: 6
+
+namespace UTCalculator
+{
+    public class CalculatorFeatures
+    {
+        public static int AddTwoNumbers(int num1, int num2)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+    }
+}
