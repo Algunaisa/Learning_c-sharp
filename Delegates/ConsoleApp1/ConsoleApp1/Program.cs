a7199f989ba4d1dc72b3ff29279ebd2a3dfa21de
Isa Flores <65521282+Algunaisa@users.noreply.github.com>
2025-11-20 14:29:23 -0600

    Add files via upload
    Se agrego ejemplos de delegacion de un curso en c#

---
+++ Delegates/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class DemoClass
+{
+    public void Method1()
+    {
+        Console.WriteLine("Method 1");
+    }
+}
+
+class DemoMain
+{
+    static void Main()
+    {
+        DemoClass myClass = new DemoClass();
+        myClass.Method1();
+    }
+}
